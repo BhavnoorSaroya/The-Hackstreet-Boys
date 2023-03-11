099d5d436a2f4d6150b1ff498c2c1101faac203d
Yousuf Rabani <mrabani@my.bcit.ca>
2023-03-11 13:21:40 -0800

    Change windows.location to windows.open for each click event listeners

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -12,21 +12,22 @@ var jaspLink = document.getElementById('jasperLink');
 var abiLink = document.getElementById('abiLink');
 
 bhavLink.addEventListener('click', ()=> {
-    window.location.href = "https://www.linkedin.com/in/bhavnoor-saroya/";
+    window.open("https://www.linkedin.com/in/bhavnoor-saroya/");
 });
 
 jasLink.addEventListener("click", ()=>{
-    window.location.href = "https://www.linkedin.com/in/jaskirat-singh-46b506159/";
+    window.open("https://www.linkedin.com/in/jaskirat-singh-46b506159/")
 });
 
 yousLink.addEventListener("click", ()=> {
-    window.location.href = "https://www.linkedin.com/in/m-yousuf-rabani-9a509364/";
+    window.open("https://www.linkedin.com/in/m-yousuf-rabani-9a509364/");
 })
 
 jaspLink.addEventListener("click", ()=> {
-    window.location.href = "https://www.linkedin.com/in/jasper-oh-595586124/";
+    window.open("https://www.linkedin.com/in/jasper-oh-595586124/");
+   
 });
 
 abiLink.addEventListener("click", ()=> {
-    window.location.href = "https://www.linkedin.com/in/abhishekchouhannk/";
+    window.open("https://www.linkedin.com/in/abhishekchouhannk/")
 })
