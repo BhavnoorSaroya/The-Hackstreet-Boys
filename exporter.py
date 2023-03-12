7a98fb6a507f5c96e23847257cb211b871de6bc1
Jasper Oh <ohyj0906@gmail.com>
2023-03-12 06:25:17 -0700

    csv export modify

--- exporter.py
+++ exporter.py
@@ -4,7 +4,7 @@ def save_to_file(jobs):
     print("got jobs")
     job_file = open("jobs.csv", mode="w")
     writer = csv.writer(job_file)
-    writer.writerow(["Title", "Company", "Apply link", "Platform" , "Did I apply it?" , "Interview Date"])
+    writer.writerow(["Title", "Company", "Apply link", "Platform" , "Did I apply it?" , "Interview Date" , "Result"])
     for job in jobs:
         writer.writerow(list(job.values()))
     return
